4da2f6152cb6ddbb984cfc452cc5c68d51557aaf
CodeWringer <nh@lutzhaase.com>
2021-10-24 18:32:30 +0200

    Adding skills

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -25,10 +25,12 @@ export class AmbersteelActorSheet extends ActorSheet {
     // sheets are the actor object, the data object, whether or not it's
     // editable, the items array, and the effects array.
     const context = super.getData();
-    console.log(context);
 
     // Use a safe clone of the actor data for further operations.
     const actorData = context.actor.data;
+
+    actorData.data.skills = context.items.filter(function(item) { return item.type == "skill" && item.data.isLearning == "false" });
+    actorData.data.learningSkills = context.items.filter(function(item) { return item.type == "skill" && item.data.isLearning == "true" });
 
     // Add the actor's data to context.data for easier access, as well as flags.
     context.data = actorData.data;
@@ -49,60 +51,33 @@ export class AmbersteelActorSheet extends ActorSheet {
    *
    * @return {undefined}
    */
-  _prepareCharacterData(context) {
-    // Handle ability scores.
-    for (let [k, v] of Object.entries(context.data.abilities)) {
-      v.label = game.i18n.localize(CONFIG.BOILERPLATE.abilities[k]) ?? k;
-    }
-  }
-
-  /**
-   * Organize and classify Items for Character sheets.
-   *
-   * @param {Object} actorData The actor to prepare.
-   *
-   * @return {undefined}
-   */
   _prepareItems(context) {
     // Initialize containers.
-    const gear = [];
-    const features = [];
-    const spells = {
-      0: [],
-      1: [],
-      2: [],
-      3: [],
-      4: [],
-      5: [],
-      6: [],
-      7: [],
-      8: [],
-      9: []
-    };
+    const possessions = [];
+    const skills = [];
+    const learningSkills = [];
 
     // Iterate through items, allocating to containers
     for (let i of context.items) {
       i.img = i.img || DEFAULT_TOKEN;
-      // Append to gear.
+      // Possessions
       if (i.type === 'item') {
-        gear.push(i);
+        possessions.push(i);
       }
-      // Append to features.
-      else if (i.type === 'feature') {
-        features.push(i);
-      }
-      // Append to spells.
-      else if (i.type === 'spell') {
-        if (i.data.spellLevel != undefined) {
-          spells[i.data.spellLevel].push(i);
+      // Skills
+      else if (i.type === 'skill') {
+        if (i.isLearning) {
+          learningSkills.push(i);
+        } else {
+          skills.push(i);
         }
       }
     }
 
     // Assign and return
-    context.gear = gear;
-    context.features = features;
-    context.spells = spells;
+    context.skills = skills;
+    context.learningSkills = learningSkills;
+    context.possessions = possessions;
    }
 
   /* -------------------------------------------- */
@@ -122,19 +97,16 @@ export class AmbersteelActorSheet extends ActorSheet {
     // Everything below here is only needed if the sheet is editable
     if (!this.isEditable) return;
 
-    // Add Inventory Item
+    // Add Item
     html.find('.item-create').click(this._onItemCreate.bind(this));
 
-    // Delete Inventory Item
+    // Delete Item
     html.find('.item-delete').click(ev => {
       const li = $(ev.currentTarget).parents(".item");
       const item = this.actor.items.get(li.data("itemId"));
       item.delete();
       li.slideUp(200, () => this.render(false));
     });
-
-    // Rollable abilities.
-    html.find('.rollable').click(this._onRoll.bind(this));
 
     // Drag events for macros.
     if (this.actor.isOwner) {
