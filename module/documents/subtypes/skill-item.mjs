8cb53c64ba48a86f8e730934dd9a32f70c2cb4d8
CodeWringer <nh@lutzhaase.com>
2021-11-04 18:50:11 +0100

    WIP skill item

---
+++ module/documents/subtypes/skill-item.mjs
@@ -0,0 +1,35 @@
+import BaseItem from "./base-item.mjs";
+
+export default class SkillItem extends BaseItem {
+    /** @override */
+    get img() { return "icons/svg/book.svg"; }
+
+    /** @override */
+    prepareData() {
+      this.owner.data.img = this.img;
+    }
+
+    /** @override */
+    async getChatData() {
+        // TODO
+        // const messageBase = super.getChatData();
+        // const messageTemplate = "systems/ambersteel/templates/item/parts/fate-card.hbs";
+        // const renderedContent = await renderTemplate(messageTemplate, {
+        //     data: {
+        //         _id: this.owner.id,
+        //         name: this.owner.name,
+        //         data: {
+        //             description: this.owner.data.data.description,
+        //         }
+        //     },
+        //     img: this.owner.img,
+        //     isEditable: false
+        // });
+
+        // return {
+        //     ...messageBase,
+        //     flavor: game.i18n.localize("ambersteel.fateSystem.fateCard"),
+        //     renderedContent: renderedContent
+        // }
+    }
+}
