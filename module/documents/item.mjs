2e61f48cbcea2fec5f81de9db2e3d08a68be759a
CodeWringer <nh@lutzhaase.com>
2021-11-04 18:49:58 +0100

    WIP Base item

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1,25 +1,34 @@
+import BaseItem from './subtypes/base-item.mjs';
 import FateCardItem from './subtypes/fate-card-item.mjs';
+import SkillItem from './subtypes/skill-item.mjs';
 
 /**
  * @extends {Item}
  */
 export class AmbersteelItem extends Item {
+  /**
+   * Type-dependent object which pseudo-extends the logic of this object. 
+   */
   subType = undefined;
 
   prepareData() {
     super.prepareData();
     const type = this.data.type;
 
-    this.data.img = "icons/svg/item-bag.svg";
     if (type === "skill") {
-      this.data.img = "icons/svg/book.svg";
+      this.subType = new SkillItem(this);
     } else if (type ==="fate-card") {
       this.subType = new FateCardItem(this);
-
-      this.data.img = this.subType.img;
+    } else {
+      this.subType = new BaseItem(this);
     }
+
+    this.subType.prepareData();
   }
 
+  /**
+   * Sends this item to chat. 
+   */
   async sendToChat() {
     return this.subType.sendToChat();
   }
