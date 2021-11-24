2e26f465f8530e9c422297fdd712648bf91eba50
CodeWringer <nh@lutzhaase.com>
2021-11-24 01:51:31 +0100

    Added missing input for damageType to skill ability list and re-arranged inputs

--- module/config.js
+++ module/config.js
@@ -34,14 +34,46 @@ ambersteel.shieldTypes = {
 }
 
 ambersteel.damageTypes = {
-    burning: "ambersteel.damageTypes.burning",
-    freezing: "ambersteel.damageTypes.freezing",
-    slashing: "ambersteel.damageTypes.slashing",
-    piercing: "ambersteel.damageTypes.piercing",
-    bludgeoning: "ambersteel.damageTypes.bludgeoning",
-    crushing: "ambersteel.damageTypes.crushing",
-    poison: "ambersteel.damageTypes.poison",
-    acid: "ambersteel.damageTypes.acid",
+    burning: {
+        name: "burning",
+        localizableName: "ambersteel.damageTypes.burning",
+        localizableAbbreviation: "ambersteel.damageTypes.burningAbbreviation"
+    },
+    freezing: {
+        name: "freezing",
+        localizableName: "ambersteel.damageTypes.freezing",
+        localizableAbbreviation: "ambersteel.damageTypes.freezingAbbreviation"
+    },
+    slashing: {
+        name: "slashing",
+        localizableName: "ambersteel.damageTypes.slashing",
+        localizableAbbreviation: "ambersteel.damageTypes.slashingAbbreviation"
+    },
+    piercing: {
+        name: "piercing",
+        localizableName: "ambersteel.damageTypes.piercing",
+        localizableAbbreviation: "ambersteel.damageTypes.piercingAbbreviation"
+    },
+    bludgeoning: {
+        name: "bludgeoning",
+        localizableName: "ambersteel.damageTypes.bludgeoning",
+        localizableAbbreviation: "ambersteel.damageTypes.bludgeoningAbbreviation"
+    },
+    crushing: {
+        name: "crushing",
+        localizableName: "ambersteel.damageTypes.crushing",
+        localizableAbbreviation: "ambersteel.damageTypes.crushingAbbreviation"
+    },
+    poison: {
+        name: "poison",
+        localizableName: "ambersteel.damageTypes.poison",
+        localizableAbbreviation: "ambersteel.damageTypes.poisonAbbreviation"
+    },
+    acid: {
+        name: "acid",
+        localizableName: "ambersteel.damageTypes.acid",
+        localizableAbbreviation: "ambersteel.damageTypes.acidAbbreviation"
+    },
 }
 
 ambersteel.attackTypes = {
