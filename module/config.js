c512e09db7687a5f2087a557b9da43088705c7f9
Nicolas Haase <nh@lutzhaase.com>
2022-11-27 16:38:19 +0100

    Issue/5: refactor: Updated references to localization keys

--- module/config.js
+++ module/config.js
@@ -3,105 +3,105 @@ export const ambersteel = {}
 ambersteel.weaponTypes = {
     unarmed: {
         name: "unarmed",
-        localizableName: "ambersteel.weaponTypes.unarmed"
+        localizableName: "ambersteel.character.asset.type.weapon.unarmed"
     },
     shortBlade: {
         name: "shortBlade",
-        localizableName: "ambersteel.weaponTypes.shortBlade"
+        localizableName: "ambersteel.character.asset.type.weapon.shortBlade"
     },
     longBlade: {
         name: "longBlade",
-        localizableName: "ambersteel.weaponTypes.longBlade"
+        localizableName: "ambersteel.character.asset.type.weapon.longBlade"
     },
     greatBlade: {
         name: "greatBlade",
-        localizableName: "ambersteel.weaponTypes.greatBlade"
+        localizableName: "ambersteel.character.asset.type.weapon.greatBlade"
     },
     axe: {
         name: "axe",
-        localizableName: "ambersteel.weaponTypes.axe"
+        localizableName: "ambersteel.character.asset.type.weapon.axe"
     },
     greatAxe: {
         name: "greatAxe",
-        localizableName: "ambersteel.weaponTypes.greatAxe"
+        localizableName: "ambersteel.character.asset.type.weapon.greatAxe"
     },
     spear: {
         name: "spear",
-        localizableName: "ambersteel.weaponTypes.spear"
+        localizableName: "ambersteel.character.asset.type.weapon.spear"
     },
     lance: {
         name: "lance",
-        localizableName: "ambersteel.weaponTypes.lance"
+        localizableName: "ambersteel.character.asset.type.weapon.lance"
     },
     polearm: {
         name: "polearm",
-        localizableName: "ambersteel.weaponTypes.polearm"
+        localizableName: "ambersteel.character.asset.type.weapon.polearm"
     },
     club: {
         name: "club",
-        localizableName: "ambersteel.weaponTypes.club"
+        localizableName: "ambersteel.character.asset.type.weapon.club"
     },
     smallCrusher: {
         name: "smallCrusher",
-        localizableName: "ambersteel.weaponTypes.smallCrusher"
+        localizableName: "ambersteel.character.asset.type.weapon.smallCrusher"
     },
     largeCrusher: {
         name: "largeCrusher",
-        localizableName: "ambersteel.weaponTypes.largeCrusher"
+        localizableName: "ambersteel.character.asset.type.weapon.largeCrusher"
     },
     shortBow: {
         name: "shortBow",
-        localizableName: "ambersteel.weaponTypes.shortBow"
+        localizableName: "ambersteel.character.asset.type.weapon.shortBow"
     },
     longBow: {
         name: "longBow",
-        localizableName: "ambersteel.weaponTypes.longBow"
+        localizableName: "ambersteel.character.asset.type.weapon.longBow"
     },
     warBow: {
         name: "warBow",
-        localizableName: "ambersteel.weaponTypes.warBow"
+        localizableName: "ambersteel.character.asset.type.weapon.warBow"
     },
     crossbow: {
         name: "crossbow",
-        localizableName: "ambersteel.weaponTypes.crossbow"
+        localizableName: "ambersteel.character.asset.type.weapon.crossbow"
     },
     firearm: {
         name: "firearm",
-        localizableName: "ambersteel.weaponTypes.firearm"
+        localizableName: "ambersteel.character.asset.type.weapon.firearm"
     }
 };
 
 ambersteel.armorTypes = {
     light: {
         name: "light",
-        localizableName: "ambersteel.armorTypes.light"
+        localizableName: "ambersteel.character.asset.type.armor.light"
     },
     medium: {
         name: "medium",
-        localizableName: "ambersteel.armorTypes.medium"
+        localizableName: "ambersteel.character.asset.type.armor.medium"
     },
     heavy: {
         name: "heavy",
-        localizableName: "ambersteel.armorTypes.heavy"
+        localizableName: "ambersteel.character.asset.type.armor.heavy"
     }
 };
 
 ambersteel.shieldTypes = {
     buckler: {
         name: "buckler",
-        localizableName: "ambersteel.shieldTypes.buckler"
+        localizableName: "ambersteel.character.asset.type.shield.buckler"
     },
     roundShield: {
         name: "roundShield",
-        localizableName: "ambersteel.shieldTypes.roundShield"
+        localizableName: "ambersteel.character.asset.type.shield.roundShield"
     },
     heaterShield: {
         name: "heaterShield",
-        localizableName: "ambersteel.shieldTypes.heaterShield"
+        localizableName: "ambersteel.character.asset.type.shield.heaterShield"
     },
     kiteShield: {
         name: "kiteShield",
-        localizableName: "ambersteel.shieldTypes.kiteShield"
+        localizableName: "ambersteel.character.asset.type.shield.kiteShield"
     }
 };
 
@@ -113,43 +113,43 @@ ambersteel.damageTypes = {
     },
     burning: {
         name: "burning",
-        localizableName: "ambersteel.damageTypes.burning",
-        localizableAbbreviation: "ambersteel.damageTypes.burningAbbreviation"
+        localizableName: "ambersteel.damageType.burning.label",
+        localizableAbbreviation: "ambersteel.damageType.burning.abbreviation"
     },
     freezing: {
         name: "freezing",
-        localizableName: "ambersteel.damageTypes.freezing",
-        localizableAbbreviation: "ambersteel.damageTypes.freezingAbbreviation"
+        localizableName: "ambersteel.damageType.freezing.label",
+        localizableAbbreviation: "ambersteel.damageType.freezing.abbreviation"
     },
     slashing: {
         name: "slashing",
-        localizableName: "ambersteel.damageTypes.slashing",
-        localizableAbbreviation: "ambersteel.damageTypes.slashingAbbreviation"
+        localizableName: "ambersteel.damageType.slashing.label",
+        localizableAbbreviation: "ambersteel.damageType.slashing.abbreviation"
     },
     piercing: {
         name: "piercing",
-        localizableName: "ambersteel.damageTypes.piercing",
-        localizableAbbreviation: "ambersteel.damageTypes.piercingAbbreviation"
+        localizableName: "ambersteel.damageType.piercing.label",
+        localizableAbbreviation: "ambersteel.damageType.piercing.abbreviation"
     },
     bludgeoning: {
         name: "bludgeoning",
-        localizableName: "ambersteel.damageTypes.bludgeoning",
-        localizableAbbreviation: "ambersteel.damageTypes.bludgeoningAbbreviation"
+        localizableName: "ambersteel.damageType.bludgeoning.label",
+        localizableAbbreviation: "ambersteel.damageType.bludgeoning.abbreviation"
     },
     crushing: {
         name: "crushing",
-        localizableName: "ambersteel.damageTypes.crushing",
-        localizableAbbreviation: "ambersteel.damageTypes.crushingAbbreviation"
+        localizableName: "ambersteel.damageType.crushing.label",
+        localizableAbbreviation: "ambersteel.damageType.crushing.abbreviation"
     },
     poison: {
         name: "poison",
-        localizableName: "ambersteel.damageTypes.poison",
-        localizableAbbreviation: "ambersteel.damageTypes.poisonAbbreviation"
+        localizableName: "ambersteel.damageType.poison.label",
+        localizableAbbreviation: "ambersteel.damageType.poison.abbreviation"
     },
     acid: {
         name: "acid",
-        localizableName: "ambersteel.damageTypes.acid",
-        localizableAbbreviation: "ambersteel.damageTypes.acidAbbreviation"
+        localizableName: "ambersteel.damageType.acid.label",
+        localizableAbbreviation: "ambersteel.damageType.acid.abbreviation"
     },
 };
 
@@ -161,18 +161,18 @@ ambersteel.attackTypes = {
     },
     singleTarget: {
         name: "singleTarget",
-        localizableName: "ambersteel.attackTypes.singleTarget",
-        localizableAbbreviation: "ambersteel.attackTypes.singleTargetAbbreviation"
+        localizableName: "ambersteel.attackType.singleTarget.label",
+        localizableAbbreviation: "ambersteel.attackType.singleTarget.abbreviation"
     },
     areaOfEffect: {
         name: "areaOfEffect",
-        localizableName: "ambersteel.attackTypes.areaOfEffect",
-        localizableAbbreviation: "ambersteel.attackTypes.areaOfEffectAbbreviation"
+        localizableName: "ambersteel.attackType.areaOfEffect.label",
+        localizableAbbreviation: "ambersteel.attackType.areaOfEffect.abbreviation"
     },
     multipleSingleTarget: {
         name: "multipleSingleTarget",
-        localizableName: "ambersteel.attackTypes.multipleSingleTarget",
-        localizableAbbreviation: "ambersteel.attackTypes.multipleSingleTargetAbbreviation"
+        localizableName: "ambersteel.attackType.multipleSingleTarget.label",
+        localizableAbbreviation: "ambersteel.attackType.multipleSingleTarget.abbreviation"
     }
 };
 
@@ -180,82 +180,82 @@ ambersteel.character = {
     attributes: {
         agility: {
             name: "agility",
-            localizableName: "ambersteel.attributes.agility",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.agility"
+            localizableName: "ambersteel.character.attribute.agility.label",
+            localizableAbbreviation: "ambersteel.character.attribute.agility.abbreviation"
         },
         endurance: {
             name: "endurance",
-            localizableName: "ambersteel.attributes.endurance",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.endurance"
+            localizableName: "ambersteel.character.attribute.endurance.label",
+            localizableAbbreviation: "ambersteel.character.attribute.endurance.abbreviation"
         },
         perception: {
             name: "perception",
-            localizableName: "ambersteel.attributes.perception",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.perception"
+            localizableName: "ambersteel.character.attribute.perception.label",
+            localizableAbbreviation: "ambersteel.character.attribute.perception.abbreviation"
         },
         strength: {
             name: "strength",
-            localizableName: "ambersteel.attributes.strength",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.strength"
+            localizableName: "ambersteel.character.attribute.strength.label",
+            localizableAbbreviation: "ambersteel.character.attribute.strength.abbreviation"
         },
         toughness: {
             name: "toughness",
-            localizableName: "ambersteel.attributes.toughness",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.toughness"
+            localizableName: "ambersteel.character.attribute.toughness.label",
+            localizableAbbreviation: "ambersteel.character.attribute.toughness.abbreviation"
         },
         intelligence: {
             name: "intelligence",
-            localizableName: "ambersteel.attributes.intelligence",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.intelligence"
+            localizableName: "ambersteel.character.attribute.intelligence.label",
+            localizableAbbreviation: "ambersteel.character.attribute.intelligence.abbreviation"
         },
         wisdom: {
             name: "wisdom",
-            localizableName: "ambersteel.attributes.wisdom",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.wisdom"
+            localizableName: "ambersteel.character.attribute.wisdom.label",
+            localizableAbbreviation: "ambersteel.character.attribute.wisdom.abbreviation"
         },
         arcana: {
             name: "arcana",
-            localizableName: "ambersteel.attributes.arcana",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.arcana"
+            localizableName: "ambersteel.character.attribute.arcana.label",
+            localizableAbbreviation: "ambersteel.character.attribute.arcana.abbreviation"
         },
         empathy: {
             name: "empathy",
-            localizableName: "ambersteel.attributes.empathy",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.empathy"
+            localizableName: "ambersteel.character.attribute.empathy.label",
+            localizableAbbreviation: "ambersteel.character.attribute.empathy.abbreviation"
         },
         oratory: {
             name: "oratory",
-            localizableName: "ambersteel.attributes.oratory",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.oratory"
+            localizableName: "ambersteel.character.attribute.oratory.label",
+            localizableAbbreviation: "ambersteel.character.attribute.oratory.abbreviation"
         },
         willpower: {
             name: "willpower",
-            localizableName: "ambersteel.attributes.willpower",
-            localizableAbbreviation: "ambersteel.attributeAbbreviations.willpower"
+            localizableName: "ambersteel.character.attribute.willpower.label",
+            localizableAbbreviation: "ambersteel.character.attribute.willpower.abbreviation"
         }
     },
     biography: {
-        origin: "ambersteel.biography.origin",
-        youth: "ambersteel.biography.youth",
-        lastFewYears: "ambersteel.biography.lastFewYears"
+        origin: "ambersteel.character.biography.origin",
+        youth: "ambersteel.character.biography.youth",
+        lastFewYears: "ambersteel.character.biography.lastFewYears"
     },
     personals: {
-        species: "ambersteel.personals.species",
-        culture: "ambersteel.personals.culture",
-        sex: "ambersteel.personals.sex",
-        age: "ambersteel.personals.age"
+        species: "ambersteel.character.personals.species",
+        culture: "ambersteel.character.personals.culture",
+        sex: "ambersteel.character.personals.sex",
+        age: "ambersteel.character.personals.age"
     },
     beliefSystem: {
-        ambition: "ambersteel.beliefSystem.ambition",
-        beliefs: "ambersteel.beliefSystem.beliefs",
-        instincts: "ambersteel.beliefSystem.instincts"
+        ambition: "ambersteel.character.beliefSystem.fateSystem.ambition",
+        beliefs: "ambersteel.character.beliefSystem.fateSystem.belief.plural",
+        instincts: "ambersteel.character.beliefSystem.fateSystem.instinct.plural"
     }
 };
 
 ambersteel.character.attributeGroups = {
     physical: {
         name: "physical",
-        localizableName: "ambersteel.attributeGroups.physical",
+        localizableName: "ambersteel.character.attributeGroup.physical.label",
         attributes: {
             agility: ambersteel.character.attributes.agility,
             endurance: ambersteel.character.attributes.endurance,
@@ -266,7 +266,7 @@ ambersteel.character.attributeGroups = {
     },
     mental: {
         name: "mental",
-        localizableName: "ambersteel.attributeGroups.mental",
+        localizableName: "ambersteel.character.attributeGroup.mental.label",
         attributes: {
             intelligence: ambersteel.character.attributes.intelligence,
             wisdom: ambersteel.character.attributes.wisdom,
@@ -275,7 +275,7 @@ ambersteel.character.attributeGroups = {
     },
     social: {
         name: "social",
-        localizableName: "ambersteel.attributeGroups.social",
+        localizableName: "ambersteel.character.attributeGroup.social.label",
         attributes: {
             empathy: ambersteel.character.attributes.empathy,
             oratory: ambersteel.character.attributes.oratory,
@@ -291,37 +291,37 @@ ambersteel.fateSystem = {
 ambersteel.visibilityModes = {
     public: {
         name: "public",
-        localizableName: "ambersteel.messageVisibilityModes.public"
+        localizableName: "ambersteel.general.messageVisibility.public.label"
     },
     self: {
         name: "self",
-        localizableName: "ambersteel.messageVisibilityModes.self"
+        localizableName: "ambersteel.general.messageVisibility.self.label"
     },
     gm: {
         name: "gm",
-        localizableName: "ambersteel.messageVisibilityModes.gm"
+        localizableName: "ambersteel.general.messageVisibility.gm.label"
     }
 };
 
 ambersteel.injuryStates = {
     active: {
         name: "active",
-        localizableName: "ambersteel.injuryStates.active",
+        localizableName: "ambersteel.character.health.injury.state.active.label",
         icon: "fas fa-bone"
     },
     patchedUp: {
         name: "patchedUp",
-        localizableName: "ambersteel.injuryStates.patchedUp",
+        localizableName: "ambersteel.character.health.injury.state.patchedUp.label",
         icon: "fas fa-band-aid"
     },
     treated: {
         name: "treated",
-        localizableName: "ambersteel.injuryStates.treated",
+        localizableName: "ambersteel.character.health.injury.state.treated.label",
         icon: "fas fa-mortar-pestle"
     },
     permanent: {
         name: "permanent",
-        localizableName: "ambersteel.injuryStates.permanent",
+        localizableName: "ambersteel.character.health.injury.state.permanent.label",
         icon: "fas fa-crutch"
     }
 };
@@ -329,12 +329,12 @@ ambersteel.injuryStates = {
 ambersteel.illnessStates = {
     active: {
         name: "active",
-        localizableName: "ambersteel.illnessStates.active",
+        localizableName: "ambersteel.character.health.illness.state.active.label",
         icon: "fas fa-virus"
     },
     treated: {
         name: "treated",
-        localizableName: "ambersteel.illnessStates.treated",
+        localizableName: "ambersteel.character.health.illness.state.treated.label",
         icon: "fas fa-mortar-pestle"
     }
 };
