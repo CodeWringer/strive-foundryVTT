8b8a5acc4d4390e12a14eed4124d25c942c57b11
Nicolas Haase <nh@lutzhaase.com>
2022-11-30 07:48:21 +0100

    wip

---
+++ module/sheets/actor/actor-sheet-subtype.mjs
@@ -0,0 +1,6 @@
+/**
+ * A map of specific actor sheet "sub-type" names and a corresponding instance of their "type". 
+ * @type {Map<String, AmbersteelBaseActorSheet>}
+ * @readonly
+ */
+export const ACTOR_SHEET_SUBTYPE = new Map();
