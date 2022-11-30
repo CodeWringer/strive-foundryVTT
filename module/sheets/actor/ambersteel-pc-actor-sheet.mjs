0c80ca3a44340d0ef253de15950c592fbbe69042
Nicolas Haase <nh@lutzhaase.com>
2022-11-30 07:48:27 +0100

    wip

--- module/sheets/actor/ambersteel-pc-actor-sheet.mjs
+++ module/sheets/actor/ambersteel-pc-actor-sheet.mjs
@@ -1,5 +1,9 @@
+import { ACTOR_SHEET_SUBTYPE } from "./actor-sheet-subtype.mjs";
 import AmbersteelBaseActorSheet from "./ambersteel-base-actor-sheet.mjs";
 
 export default class AmbersteelPcActorSheet extends AmbersteelBaseActorSheet {
-  // Currently has no specific behavior. 
+  /** @override */
+  get title() { return game.i18n.localize("ambersteel.general.actor.pc.label"); }
 }
+
+ACTOR_SHEET_SUBTYPE.set("pc", new AmbersteelPcActorSheet());
