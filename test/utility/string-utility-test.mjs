becbe8502fd8ca13ecd4480520ec6bd63cf9ae5d
Nicolas Haase <nh@lutzhaase.com>
2022-12-01 21:28:34 +0100

    Issue/68: feature: 2 more unit tests

--- test/utility/string-utility-test.mjs
+++ test/utility/string-utility-test.mjs
@@ -22,5 +22,28 @@ describe('string-utility', function() {
       // Then
       r.should.be.equal("Hello, Bob! Goodbye, Bobette!");
     });
+
+    it('not enough arguments outputs "Hello, Bob! Goodbye, %s!"', function() {
+      // Given
+      const given = "Hello, %s! Goodbye, %s!";
+      const givenReplacement1 = "Bob";
+      // When
+      const r = StringUtil.format(given, givenReplacement1);
+      // Then
+      r.should.be.equal("Hello, Bob! Goodbye, %s!");
+    });
+
+    it('too many arguments outputs "Hello, Bob! Goodbye, Bobette!"', function() {
+      // Given
+      const given = "Hello, %s! Goodbye, %s!";
+      const givenReplacement1 = "Bob";
+      const givenReplacement2 = "Bobette";
+      const givenReplacement3 = "Dave";
+      const givenReplacement4 = "Anton";
+      // When
+      const r = StringUtil.format(given, givenReplacement1, givenReplacement2, givenReplacement3, givenReplacement4);
+      // Then
+      r.should.be.equal("Hello, Bob! Goodbye, Bobette!");
+    });
   });
 });
