f27ea89385d7459057b9c6b57c12991848d26f60
Nicolas Haase <nh@lutzhaase.com>
2022-12-06 00:09:20 +0100

    Issue/129: fix: JsDoc in inputLabel

--- module/components/label/label-viewmodel.mjs
+++ module/components/label/label-viewmodel.mjs
@@ -1,5 +1,7 @@
 import { TEMPLATES } from "../../templatePreloader.mjs";
 import ViewModel from "../viewmodel.mjs";
+import ViewModelTypeDefinition from "../../view-model/view-model-type-definition.mjs";
+import { VIEW_MODEL_TYPE } from "../../view-model/view-model-type.mjs";
 
 /**
  * --- Inherited from ViewModel
@@ -10,28 +12,24 @@ import ViewModel from "../viewmodel.mjs";
  * 
  * --- Own properties
  * 
- * @property {String} text Optional. The localization key of the text to display. 
- * @property {String} for Optional. The id of the associated input component. 
- * @property {String} cssClass Optional. A custom css class. 
- * 
  */
 export default class LabelViewModel extends ViewModel {
   static get TEMPLATE() { return TEMPLATES.COMPONENT_INPUT_LABEL; }
 
   /**
    * @param {String | undefined} args.id Optional. Unique ID of this view model instance. 
-   * 
-   * @param {String | undefined} args.text Optional. The localization key of the text to display. 
-   * @param {String | undefined} args.for Optional. The id of the associated input component. 
-   * @param {String | undefined} args.cssClass Optional. A custom css class. 
    */
   constructor(args = {}) {
     super(args);
-
-    this.text = args.text ?? "";
-    this.for = args.for ?? "";
-    this.cssClass = args.cssClass;
   }
 }
 
 Handlebars.registerPartial('inputLabel', `{{> "${LabelViewModel.TEMPLATE}"}}`);
+
+VIEW_MODEL_TYPE.set(
+  "LabelViewModel", 
+  new ViewModelTypeDefinition(
+    (args) => { return new LabelViewModel(args); },
+    []
+  )
+);
