ef9d6e067f1f300d57094be7fa7b653eb2c81a54
Nicolas Haase <nh@lutzhaase.com>
2022-12-07 00:43:03 +0100

    Issue/160: refactor: wip: Complete project re-structure
    * wip import fix-up

--- business/document/actor/actor.mjs
+++ business/document/actor/actor.mjs
@@ -4,12 +4,12 @@ import AmbersteelNpcActor from './ambersteel-npc-actor.mjs';
 import AmbersteelPlainActor from './ambersteel-plain-actor.mjs';
 import { ACTOR_SUBTYPE } from './actor-subtype.mjs';
 // Other imports
-import ActorChatMessageViewModel from '../../../templates/actor/actor-chat-message-viewmodel.mjs';
-import PreparedChatData from '../../dto/prepared-chat-data.mjs';
-import * as UpdateUtil from '../../utils/document-update-utility.mjs';
-import * as ChatUtil from "../../utils/chat-utility.mjs";
-import { createUUID } from '../../utils/uuid-utility.mjs';
-import { SOUNDS_CONSTANTS } from '../../constants/sounds.mjs';
+import ActorChatMessageViewModel from '../../../presentation/template/actor/actor-chat-message-viewmodel.mjs';
+import PreparedChatData from '../../../presentation/chat/prepared-chat-data.mjs';
+import * as UpdateUtil from '../document-update-utility.mjs';
+import * as ChatUtil from "../../presentation/chat/chat-utility.mjs";
+import { createUUID } from '../../util/uuid-utility.mjs';
+import { SOUNDS_CONSTANTS } from '../../../presentation/audio/sounds.mjs';
 
 /**
  * @summary
