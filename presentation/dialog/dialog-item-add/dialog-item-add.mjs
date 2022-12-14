74e2e16f3dbae62a10622fb5b7abff21708343e1
Nicolas Haase <nh@lutzhaase.com>
2022-12-14 14:12:43 +0100

    Issue/158: refactor: The add item dialog now uses the new dialog class

--- presentation/dialog/dialog-item-add/dialog-item-add.mjs
+++ presentation/dialog/dialog-item-add/dialog-item-add.mjs
@@ -4,7 +4,6 @@ import { validateOrThrow } from '../../../business/util/validation-utility.mjs';
 import { getElementValue } from '../../sheet/sheet-utility.mjs';
 import { TEMPLATES } from '../../template/templatePreloader.mjs';
 import ConfirmableModalDialog from '../confirmable-modal-dialog/confirmable-modal-dialog.mjs';
-import * as DialogUtil from '../dialog-utility.mjs';
 
 const LOCALIZABLE_TITLE = "ambersteel.general.select";
 
@@ -38,7 +37,7 @@ export default class AddItemDialog extends ConfirmableModalDialog {
    * @readonly
    * @type {Boolean}
    */
-  get isCustomChecked() { return getElementValue(dialogResult.html.find(".ambersteel-is-custom")[0]); }
+  get isCustomChecked() { return getElementValue(this._html.find(".ambersteel-is-custom")[0]); }
 
   /**
    * @param {Object} options 
@@ -69,7 +68,6 @@ export default class AddItemDialog extends ConfirmableModalDialog {
   activateListeners(html) {
     super.activateListeners(html);
 
-    const thiz = this;
     html.find(".ambersteel-is-custom").change(event => {
       const select = html.find(".ambersteel-item-select")[0];
       if (getElementValue(event.currentTarget) === true) {
@@ -89,75 +87,4 @@ export default class AddItemDialog extends ConfirmableModalDialog {
       showFancyFont: this._showFancyFont,
     }
   }
-}
-
-window.AddItemDialog = AddItemDialog;
-
-/**
- * @param {String} itemType Item type. E. g. "skill" or "injury"
- * @param {String | undefined} localizableItemLabel Localization string for the item input label. 
- * @param {String | undefined} localizableTitle Localization string for the dialog title. 
- * @returns {Promise<Object>} = {
- * selected: {String} Id of the selected item,
- * isCustomChecked: {Boolean},
- * confirmed: {Boolean}
- * }
- * @async
- */
-export async function showDialog(itemType, localizableItemLabel = LOCALIZABLE_ITEM_LABEL, localizableTitle = LOCALIZABLE_TITLE) {
-  const itemDeclarations = getItemDeclarations(itemType);
-
-  return new Promise(async (resolve, reject) => {
-    const dialogResult = await DialogUtil.showDialog(
-      {
-        dialogTemplate: TEMPLATES.DIALOG_ITEM_ADD,
-        localizedTitle: game.i18n.localize(localizableTitle),
-        render: html => {
-          html.find(".ambersteel-is-custom").change(event => {
-            const select = html.find(".ambersteel-item-select")[0];
-            if (getElementValue(event.currentTarget) === true) {
-              select.className = select.className + " ambersteel-read-only";
-            } else {
-              select.className = select.className.replace(" ambersteel-read-only", "");
-            }
-          });
-        }
-      },
-      {
-        itemLabel: game.i18n.localize(localizableItemLabel),
-        itemDeclarations: itemDeclarations,
-        showFancyFont: new GetShowFancyFontUseCase().invoke(),
-      }
-    );
-    resolve({
-      selected: getElementValue(dialogResult.html.find(".ambersteel-item-select")[0]),
-      isCustomChecked: getElementValue(dialogResult.html.find(".ambersteel-is-custom")[0]),
-      confirmed: dialogResult.confirmed
-    });
-  });
-}
-
-/**
- * @param {String} itemType Item type. "skill"|"injury"|"illness"|"item"
- * @param {String | undefined} localizableItemLabel Localization string for the item input label. 
- * @param {String | undefined} localizableTitle Localization string for the dialog title. 
- * @returns {Promise<Object>} = {
- * selected: {String} Id of the selected item,
- * isCustomChecked: {Boolean},
- * confirmed: {Boolean}
- * }
- * @async
- */
-export async function query(itemType, localizableItemLabel = LOCALIZABLE_ITEM_LABEL, localizableTitle = LOCALIZABLE_TITLE) {
-  return new Promise(async (resolve, reject) => {
-    if (game.keyboard.downKeys.has("SHIFT")) {
-      resolve({
-        selected: undefined,
-        isCustomChecked: true,
-        confirmed: true
-      });
-    } else {
-      resolve(await showDialog(itemType, localizableItemLabel, localizableTitle));
-    }
-  });
 }
