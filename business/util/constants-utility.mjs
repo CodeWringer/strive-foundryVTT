3f3587748d1dd3b1913d7c1aaf9fa379c4f608af
Nicolas Haase <nh@lutzhaase.com>
2022-12-15 01:04:33 +0100

    Issue/170: feature: new constants-utility
    * This is a currently necessary utility for the sake of simplifying the process of providing `ChoiceOption`s for constants, so they're selectable via the UI.

---
+++ business/util/constants-utility.mjs
@@ -0,0 +1,35 @@
+import ChoiceOption from "../../presentation/util/choice-option.mjs";
+
+/**
+* Returns an array of `ChoiceOption`s, based on the given constants object. 
+* 
+* @param {Object} constantsObject Any constants object.
+* * **All** not explicitly excluded properties, that aren't part of the prototype, will be turned 
+* into `ChoiceOption`s. 
+* @param {Array<String> | undefined} exclude An array of property names to exclude. 
+* 
+* @returns {Array<ChoiceOption>}
+*/
+export function getAsChoices(constantsObject, exclude) {
+	const result = [];
+
+	for (const entryName in constantsObject) {
+		if (constantsObject.hasOwnProperty(entryName) !== true) continue;
+		if (exclude.find(it => it === entryName) !== undefined) continue;
+
+		const entry = constantsObject[entryName];
+		
+		const localizedName = entry.localizableName !== undefined ? game.i18n.localize(entry.localizableName) : undefined;
+		const icon = entry.icon;
+
+		result.push(new ChoiceOption({
+			value: entry.name,
+			localizedValue: localizedName,
+			icon: icon,
+			shouldDisplayValue: localizedName !== undefined ? true : false,
+			shouldDisplayIcon: icon !== undefined ? true : false,
+		}));
+	}
+
+	return result;
+}
