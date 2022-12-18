8834e1e804cf5110a2485ecc7834c049a402e1eb
Nicolas Haase <nh@lutzhaase.com>
2022-12-18 13:22:27 +0100

    Issue/154: fix: Ensure changes will be persisted during migration

--- business/migration/migrators/migrator_1-3-2_1-4-0.mjs
+++ business/migration/migrators/migrator_1-3-2_1-4-0.mjs
@@ -1,5 +1,7 @@
 import { DOCUMENT_COLLECTION_SOURCES } from "../../document/document-fetcher/document-collection-source.mjs";
 import DocumentFetcher from "../../document/document-fetcher/document-fetcher.mjs";
+import { deleteByPropertyPath } from "../../document/document-update-utility.mjs";
+import { updateProperty } from "../../document/document-update-utility.mjs";
 import AbstractMigrator from "../abstract-migrator.mjs";
 import { MIGRATORS } from "../migrators.mjs";
 import VersionCode from "../version-code.mjs";
@@ -45,8 +47,19 @@ export default class Migrator_1_3_2__1_4_0 extends AbstractMigrator {
         const attributeGroup = attributes[attributeGroupToMigrate.groupName];
 
         for (const attributeName of attributeGroupToMigrate.attributeNames) {
-          attributeGroup[attributeName].level = attributeGroup[attributeName].value;
-          delete attributeGroup[attributeName].value;
+          const attribute = attributeGroup[attributeName];
+
+          // Change in-memory data. 
+          attribute.level = attribute.value;
+          delete attribute.value;
+
+          const dataPath = `data.data.attributes.${attributeGroupToMigrate.groupName}.${attributeName}`;
+
+          // Delete property from attribute in data base. 
+          await deleteByPropertyPath(actor, `${dataPath}.value`, false);
+          
+          // Persist level to attribute in data base. 
+          await updateProperty(actor, `${dataPath}.level`, attribute.level, false);
         }
       }
     }
@@ -62,8 +75,15 @@ export default class Migrator_1_3_2__1_4_0 extends AbstractMigrator {
     // Replace "value" with "level" on every skill.
     for (const skill of skills) {
       
+      // Change in-memory data. 
       skill.data.data.level = skill.data.data.value;
       delete skill.data.data.value;
+
+      // Delete property from skill in data base. 
+      await deleteByPropertyPath(skill, "data.data.value", false);
+
+      // Persist level to skill in data base. 
+      await updateProperty(skill, "data.data.level", skill.data.data.level, false);
     }
   }
 }
