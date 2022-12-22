3c59ea5eb2d319f00802d4384ebce6251cc4a338
Nicolas Haase <nh@lutzhaase.com>
2022-12-22 15:05:18 +0100

    Issue/85: refactor: Extracted item imports

--- presentation/sheet/item/item-sheet.mjs
+++ presentation/sheet/item/item-sheet.mjs
@@ -1,19 +1,11 @@
-// Imports of specific item sheet "sub-types", to ensure their imports cause the `ITEM_SHEET_SUBTYPE` map to be populated. 
-import AmbersteelBaseItemSheet from "./ambersteel-base-item-sheet.mjs";
-import AmbersteelSkillItemSheet from "./ambersteel-skill-item-sheet.mjs";
-import AmbersteelFateCardItemSheet from "./ambersteel-fate-item-sheet.mjs";
-import AmbersteelInjuryItemSheet from "./ambersteel-injury-item-sheet.mjs";
-import AmbersteelIllnessItemSheet from "./ambersteel-illness-item-sheet.mjs";
-import AmbersteelMutationItemSheet from "./ambersteel-mutation-item-sheet.mjs";
 import { ITEM_SHEET_SUBTYPE } from "./item-sheet-subtype.mjs";
-// Other imports
 import * as SheetUtil from "../sheet-utility.mjs";
 import { SYSTEM_ID } from "../../../system-id.mjs";
 
 export class AmbersteelItemSheet extends ItemSheet {
   /**
    * Type-dependent object which pseudo-extends the logic of this object. 
-   * @type {AmbersteelBaseItemSheet | AmbersteelSkillItemSheet | AmbersteelFateCardItemSheet | AmbersteelInjuryItemSheet | AmbersteelIllnessItemSheet | AmbersteelMutationItemSheet}
+   * @type {AmbersteelBaseItemSheet}
    * @readonly
    */
   get subType() {
