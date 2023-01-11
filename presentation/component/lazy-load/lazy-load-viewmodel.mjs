413c649bffb444da7f879175298026a03e8dbefd
Nicolas Haase <nh@lutzhaase.com>
2023-01-11 17:17:49 +0100

    Issue/85: fix: Caching and event attaching now works correctly

--- presentation/component/lazy-load/lazy-load-viewmodel.mjs
+++ presentation/component/lazy-load/lazy-load-viewmodel.mjs
@@ -54,6 +54,37 @@ export default class LazyLoadViewModel extends ViewModel {
   get element() { return this._html.find(`#${this.id}`); }
 
   /**
+   * The cached DOM. 
+   * 
+   * @type {JQuery | undefined}
+   * @default undefined
+   * @private
+   */
+  _html = undefined;
+
+  /**
+   * The cached rendered content. 
+   * 
+   * Assuming the data model didn't change, the content won't have to be 
+   * re-rendered and can instead be fetched and added to the DOM, quickly. 
+   * 
+   * @type {String | undefined}
+   * @default undefined
+   * @private
+   */
+  _renderedContent = undefined;
+
+  /**
+   * If true, the underlying data was updated and the content must be 
+   * rendered again. 
+   * 
+   * @type {Boolean}
+   * @default false
+   * @private
+   */
+  _invalidated = false;
+
+  /**
    * @param {String | undefined} args.id Optional. Unique ID of this view model instance. 
    * @param {String} args.template A Handlebars template to lazy load. 
    * @param {Function} args.viewModelFactoryFunction A function that returns a view 
@@ -73,9 +104,19 @@ export default class LazyLoadViewModel extends ViewModel {
 
   /** @override */
   activateListeners(html, isOwner, isEditable) {
-    super.activateListeners(html, isOwner, isEditable);
-
+    // First of all, cache the DOM we're working with. The getters rely on it. 
     this._html = html;
+
+    // Ensure the cached rendered template is added to the DOM, *before* the 
+    // child view model may attach any event listeners on the DOM. 
+    // This must happen before the "super" call, as it relies on the DOM 
+    // to be complete. 
+    if (this._renderedContent !== undefined) {
+      this.element.empty();
+      this.element.append(this._renderedContent);
+    }
+
+    super.activateListeners(html, isOwner, isEditable);
   }
 
   /**
@@ -85,23 +126,32 @@ export default class LazyLoadViewModel extends ViewModel {
    * @async
    */
   async render() {
+    // Ensure a view model instance exists. 
     if (this.viewModel === undefined) {
       this.viewModel = this.viewModelFactoryFunction(this.viewModelArgs);
       this.viewModel.parent = this;
     }
 
+    // Render as needed. 
+    if (this._renderedContent === undefined || this._invalidated === true) {
+      this._renderedContent = await renderTemplate(this.template, {
+        viewModel: this.viewModel,
+      });
+      this._invalidated = false;
+    }
+
+    // Manipulate the DOM. 
     this.element.empty();
+    this.element.append(this._renderedContent);
 
-    const renderedContent = await renderTemplate(this.template, {
-      viewModel: this.viewModel,
-    });
-    this.element.append(renderedContent);
-
+    // *Lastly* the wrapped view model may attach its event listeners. 
     this.viewModel.activateListeners(this.element, this.viewModel.isOwner, this.viewModel.isEditable);
   }
 
   /** @override */
   update(args = {}) {
+    this._invalidated = true;
+
     if (this.viewModel !== undefined) {
       this.viewModel.update(args);
     }
