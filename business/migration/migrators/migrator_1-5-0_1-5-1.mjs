d5584e81a4fcbec187c2f3933db8c17f071e4c93
Nicolas Haase <nh@lutzhaase.com>
2023-01-13 13:57:08 +0100

    chore: Version increase

---
+++ business/migration/migrators/migrator_1-5-0_1-5-1.mjs
@@ -0,0 +1,18 @@
+import AbstractMigrator from "../abstract-migrator.mjs";
+import { MIGRATORS } from "../migrators.mjs";
+import VersionCode from "../version-code.mjs";
+
+export default class Migrator_1_5_0__1_5_1 extends AbstractMigrator {
+  /** @override */
+  get targetVersion() { return new VersionCode(1, 5, 0) };
+
+  /** @override */
+  get migratedVersion() { return new VersionCode(1, 5, 1) };
+
+  /** @override */
+  async _doWork() {
+    // Nothing yet.
+  }
+}
+
+MIGRATORS.push(new Migrator_1_5_0__1_5_1());
