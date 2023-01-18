b44c6c37f06bdd4a1007694036e4dd5933a2b2b4
Nicolas Haase <nh@lutzhaase.com>
2023-01-18 21:02:29 +0100

    Issue/204: wip

--- presentation/sheet/item/asset/asset-chat-message-viewmodel.mjs
+++ presentation/sheet/item/asset/asset-chat-message-viewmodel.mjs
@@ -1,5 +1,6 @@
 import { validateOrThrow } from "../../../../business/util/validation-utility.mjs"
 import { TAKE_ITEM_CONTEXT_TYPES } from "../../../component/button-take-item/button-take-item-viewmodel.mjs"
+import InputPropertiesViewModel from "../../../component/input-properties/input-properties-viewmodel.mjs"
 import LazyRichTextViewModel from "../../../component/lazy-rich-text/lazy-rich-text-viewmodel.mjs"
 import { TEMPLATES } from "../../../templatePreloader.mjs"
 import ViewModelFactory from "../../../view-model/view-model-factory.mjs"
@@ -80,6 +81,13 @@ export default class AssetChatMessageViewModel extends ViewModel {
       isSendable: this.isSendable,
       isOwner: this.isOwner,
       renderableContent: this.document.description,
+    });
+    this.vmProperties = new InputPropertiesViewModel({
+      id: "vmProperties",
+      parent: this,
+      propertyPath: "properties",
+      propertyOwner: this.document,
+      isEditable: false,
     });
   }
 }
