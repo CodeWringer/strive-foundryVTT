be1c92a9c525fe27f6cd4961c5694304493e1eb7
Nicolas Haase <nh@lutzhaase.com>
2023-03-09 19:11:16 +0100

    Issue/249: Custom health state limit can now be defined

--- presentation/dialog/settings/health-states/custom-health-state-list-item-viewmodel.mjs
+++ presentation/dialog/settings/health-states/custom-health-state-list-item-viewmodel.mjs
@@ -1,4 +1,6 @@
 import { validateOrThrow } from "../../../../business/util/validation-utility.mjs";
+import ObservableField from "../../../../common/observables/observable-field.mjs";
+import InputNumberSpinnerViewModel from "../../../component/input-number-spinner/input-number-spinner-viewmodel.mjs";
 import InputTextFieldViewModel from "../../../component/input-textfield/input-textfield-viewmodel.mjs";
 import { TEMPLATES } from "../../../templatePreloader.mjs";
 import ViewModel from "../../../view-model/view-model.mjs";
@@ -13,33 +15,63 @@ export default class CustomHealthStateListItemViewModel extends ViewModel {
   static get TEMPLATE() { return TEMPLATES.CUSTOM_HEALTH_STATE_LIST_ITEM; }
 
   /**
+   * Returns an object representation of the data. 
+   * 
+   * @type {Object}
+   * @private
+   * @readonly
+   */
+  get state() { return {
+      name: this.stateName.value,
+      limit: this.stateLimit.value,
+    };
+  }
+
+  /**
    * @param {Object} args
    * @param {String | undefined} args.id Unique ID of this view model instance. 
    * @param {Boolean | undefined} args.isEditable If true, input(s) will be in edit mode. If false, input(s) will be in read-only mode.
    * @param {String | undefined} args.contextTemplate Name or path of a template that embeds this input component. 
    * 
-   * @param {String} args.propertyOwner
-   * @param {String} args.propertyPath
+   * @param {String} args.stateName
+   * @param {Number | undefined} args.stateLimit
+   * * Default `0`
    * @param {Function | undefined} args.onChange Callback that is invoked when the value changes. 
-   * * Receives two arguments: 
-   * * * `oldValue: {Any}`
-   * * * `newValue: {Any}`
+   * * Receives one argument: 
+   * * * `state: {Object}`
+   * * * `state.name: {String}`
+   * * * `state.limit: {Number}`
    */
   constructor(args = {}) {
     super(args);
-    validateOrThrow(args, ["propertyOwner", "propertyPath"]);
+    validateOrThrow(args, ["stateName"]);
 
-    this.propertyOwner = args.propertyOwner;
-    this.propertyPath = args.propertyPath;
     this.onChange = args.onChange ?? (() => {});
+
+    this.stateName = new ObservableField({ value: args.stateName})
+    this.stateName.onChange((field, oldValue, newValue) => {
+      this.onChange(this.state);
+    });
+
+    this.stateLimit = new ObservableField({ value: (args.stateLimit ?? 0)})
+    this.stateLimit.onChange((field, oldValue, newValue) => {
+      this.onChange(this.state);
+    });
 
     this.vmName = new InputTextFieldViewModel({
       id: "vmName",
       parent: this,
       isEditable: this.isEditable,
-      propertyOwner: this.propertyOwner,
-      propertyPath: this.propertyPath,
-      onChange: this.onChange,
+      propertyOwner: this,
+      propertyPath: "stateName.value",
+    });
+    this.vmLimit = new InputNumberSpinnerViewModel({
+      id: "vmLimit",
+      parent: this,
+      isEditable: this.isEditable,
+      propertyOwner: this,
+      propertyPath: "stateLimit.value",
+      min: 0,
     });
   }
 }
