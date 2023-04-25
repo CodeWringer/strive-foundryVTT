ab8eeab1698b7163c2021614c112450526e60193
Nicolas Haase <nh@lutzhaase.com>
2023-04-25 12:52:45 +0200

    New migrator for v1.5.3 -> v1.5.4

---
+++ test/business/migration/migrators/migrator_1-5-3_1-5-4-test.mjs
@@ -0,0 +1,20 @@
+import should from 'should';
+import sinon from 'sinon';
+import 'should-sinon';
+import { WorldSystemVersion } from '../../../../business/migration/world-system-version.mjs';
+import VersionCode from '../../../../business/migration/version-code.mjs';
+import * as MigratorTestBase from './migrator-test-base.mjs';
+import Migrator_1_5_3__1_5_4 from '../../../../business/migration/migrators/migrator_1-5-3_1-5-4.mjs';
+
+describe("Migrator_1_5_3__1_5_4", () => {
+  it("migrates correctly", async () => {
+    // Given
+    const given = new Migrator_1_5_3__1_5_4();
+    // Setup
+    MigratorTestBase.setup("1.5.3");
+    // When
+    await given.migrate();
+    // Then
+    WorldSystemVersion.version.should.deepEqual(new VersionCode(1, 5, 4))
+  });
+});
