011d7b7e3bbf39edc44f3c96ce46d64f226c1acc
Nicolas Haase <nh@lutzhaase.com>
2023-04-25 19:05:46 +0200

    Issue/293: Improved `DYNAMIC_INPUT_TYPES` docs

--- presentation/dialog/dynamic-input-dialog/dynamic-input-types.mjs
+++ presentation/dialog/dynamic-input-dialog/dynamic-input-types.mjs
@@ -1,11 +1,25 @@
 /**
- * @property {String} DROP_DOWN
+ * @property {String} DROP_DOWN A drop-down. 
+ * * `specificArgs`: 
+ * * * `options: {Array<ChoiceOption>}` The options available to the drop-down. 
+ * * * `adapter: {ChoiceAdapter}` A `ChoiceOption` adapter. 
  * @property {String} IMAGE
  * @property {String} NUMBER_SPINNER
- * @property {String} RADIO_BUTTONS
+ * * `specificArgs`: 
+ * * * `min: {Number | undefined}` Optional. The minimum value. 
+ * * * `max: {Number | undefined}` Optional. The maximum value. 
+ * * * `step: {Number | undefined}` Optional. The increment/decrement step size. 
+ * @property {String} RADIO_BUTTONS A radio button group.
+ * * `specificArgs`: 
+ * * * `options: {Array<StatefulChoiceOption>}` The options available to the radio button group. 
  * @property {String} RICH_TEXT
  * @property {String} TEXTAREA
+ * * `specificArgs`: 
+ * * * `spellcheck: {Boolean | undefined}` Optional. Sets whether spell checking is enabled. 
+ * * * `placeholder: {String | undefined}` Optional. Sets a placeholder text to display while the textfield is empty. 
  * @property {String} TEXTFIELD
+ * * `specificArgs`: 
+ * * * `placeholder: {String | undefined}` Optional. A placeholder text to display while the textfield is empty. 
  * 
  * @constant
  */
