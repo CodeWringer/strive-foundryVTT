5ed45b40bbe3f936fa5b937ffb7c3226cd6dc9fb
Nicolas Haase <nh@lutzhaase.com>
2023-04-27 15:58:39 +0200

    wip

---
+++ test/presentation/dialog/settings/health-states/health-states-settings-dialog-test.mjs
@@ -0,0 +1,5 @@
+import should from 'should';
+import sinon from 'sinon';
+import 'should-sinon';
+global.FormApplication = function() {};
+import HealthStatesSettingsDialog from '../../../../../presentation/dialog/settings/health-states/health-states-settings-dialog.mjs';
