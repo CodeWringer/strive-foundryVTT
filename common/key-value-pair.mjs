155883ae466b99ad0ba77606bc3c6b8f35c61887
Nicolas Haase <nh@lutzhaase.com>
2023-11-13 13:39:48 +0100

    docs

--- common/key-value-pair.mjs
+++ common/key-value-pair.mjs
@@ -1,8 +1,19 @@
 /**
+ * Represents a value associated with a specific key. 
+ * 
  * @property {String | Number | Boolean} key
  * @property {String | Number | Boolean | undefined | null} value
  */
 export default class KeyValuePair {
+  /**
+   * Returns a new instance of this type, parsed from the given DTO. 
+   * 
+   * @param {Object} dto 
+   * 
+   * @returns {KeyValuePair}
+   * 
+   * @static
+   */
   static fromDto(dto) {
     return new KeyValuePair(dto.key, dto.value);
   }
@@ -16,6 +27,11 @@ export default class KeyValuePair {
     this.value = value;
   }
 
+  /**
+   * Returns a DTO representation of this object. 
+   * 
+   * @returns {Object}
+   */
   toDto() {
     return {
       key: this.key,
