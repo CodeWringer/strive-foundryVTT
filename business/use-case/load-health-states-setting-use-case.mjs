70736add3371d2cfc211070a62021e8a1fae7287
Nicolas Haase <nh@lutzhaase.com>
2024-02-05 11:46:33 +0100

    fix: Health settings dialog now reflects changes made, again
    * Also took the opportunity to reduce some clutter and fix some docs.
    * The health states dialog could use some refactoring. But that is out of scope.

--- business/use-case/load-health-states-setting-use-case.mjs
+++ business/use-case/load-health-states-setting-use-case.mjs
@@ -3,11 +3,9 @@ import AbstractUseCase from "./abstract-use-case.mjs";
 
 export default class LoadHealthStatesSettingUseCase extends AbstractUseCase {
   /**
-   * @param {*} args 
-   * 
    * @returns {Object}
    */
-  invoke(args) {
+  invoke() {
     const ds = new AmbersteelWorldSettings();
     return ds.get(AmbersteelWorldSettings.KEY_CUSTOM_HEALTH_STATES);
   }
