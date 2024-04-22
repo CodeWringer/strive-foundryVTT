b9a300324c797f38eaa3b0d324b8210862cab579
Nicolas H <nh@lutzhaase.com>
2024-04-23 00:41:28 +0200

    feature: New sort controls component

--- presentation/component/sort-controls/sort-controls-viewmodel.mjs
+++ presentation/component/sort-controls/sort-controls-viewmodel.mjs
@@ -4,7 +4,7 @@ import ViewModel from "../../view-model/view-model.mjs";
 import ButtonViewModel from "../button/button-viewmodel.mjs";
 
 /**
- * Represents a block of controls for sorting (lists).
+ * Represents a block of fire-and-forget sorting controls.
  * 
  * @extends ViewModel
  * 
@@ -14,88 +14,46 @@ export default class SortControlsViewModel extends ViewModel {
   static get TEMPLATE() { return TEMPLATES.COMPONENT_SORT_CONTROLS; }
 
   /**
-   * @type {Object}
-   * @private
-   */
-  _sortStates = {};
-
-  /**
    * @param {Object} args 
    * @param {String | undefined} args.id Optional. Unique ID of this view model instance. 
    * @param {Boolean | undefined} args.isEditable Optional. If true, input(s) will be in edit mode. If false, input(s) will be in read-only mode.
    * 
    * @param {Array<SortingDefinition> | undefined} args.definitions A list of sortable property definitions.  
-   * @param {Function | undefined} args.onClick Asynchronous callback that is invoked when a sort button is clicked. Switches the current 
-   * state to the next state, before invoking, but does **not** perform any sorting! That task is delegated to the user of this class! 
+   * @param {Function | undefined} args.onClick Asynchronous callback that is invoked when a sort button is clicked. 
+   * Does **not** perform any sorting! That task is delegated to the user of this class! 
    * Arguments: 
    * * `event: Event`
    * * `definition: SortingDefinition`
+   * * `ascending: Boolean` - If true, the user clicked the button to sort in ascending fashion. 
    */
   constructor(args = {}) {
     super(args);
 
-    // View state.
-    this.registerViewStateProperty("_sortStates");
-    this.readViewState();
-
     this.onClick = args.onClick ?? (async () => { });
     this.definitions = args.definitions ?? [];
 
-    // Replace given definition states with those (if any) retrieved from view state. 
-    this.definitions.forEach(definition => {
-      definition.state = this._sortStates[definition.id] ?? SORTING_STATE.INDETERMINATE;
-    });
-
-    this.presentableViewModels = this.definitions.map(definition => {
+    this.definitionViewModels = this.definitions.map(definition => {
       return {
         definition: definition,
-        viewModel: new ButtonViewModel({
-          id: definition.id,
+        vmSortAscending: new ButtonViewModel({
+          id: `${definition.id}-ascending`,
           parent: this,
-          localizedToolTip: definition.localizedToolTip,
-          localizedLabel: definition.localizedLabel,
-          iconHtml: definition.iconHtml,
+          localizedToolTip: definition.localizedToolTipSortAscending,
+          iconHtml: '<i class="ico interactible dark ico-ascending-solid"></i>',
           onClick: async (event) => {
-            // Switch the current state. 
-            if (definition.state === SORTING_STATE.ASCENDING) {
-              definition.state = SORTING_STATE.DESCENDING;
-            } else {
-              definition.state = SORTING_STATE.ASCENDING;
-            }
-
-            // Reset other definitions. 
-            this.definitions.forEach(otherDefinition => {
-              if (otherDefinition.id !== definition.id) {
-                otherDefinition.state = SORTING_STATE.INDETERMINATE;
-                this._sortStates[otherDefinition.id] = otherDefinition.state;
-              }
-            });
-
-            // View state update. 
-            this._sortStates[definition.id] = definition.state;
-            this.writeViewState();
-
-            // Re-render symbols. 
-            this._renderSymbols();
-
-            // Invoke callback. 
-            this.onClick(event, definition);
+            this.onClick(event, definition, true);
           },
         }),
-        symbol: definition.getSymbol(),
+        vmSortDescending: new ButtonViewModel({
+          id: `${definition.id}-descending`,
+          parent: this,
+          localizedToolTip: definition.localizedToolTipSortDescending,
+          iconHtml: '<i class="ico interactible dark ico-descending-solid"></i>',
+          onClick: async (event) => {
+            this.onClick(event, definition, false);
+          },
+        }),
       };
-    });
-  }
-
-  /**
-   * Re-renders the symbols of all definitions, based on their current state. 
-   * 
-   * @private
-   */
-  _renderSymbols() {
-    this.presentableViewModels.forEach(vm => {
-      const symbolElement = this.element.find(`#${vm.viewModel.id}-symbol`);
-      $(symbolElement).html(vm.definition.getSymbol());
     });
   }
 }
@@ -104,61 +62,30 @@ export default class SortControlsViewModel extends ViewModel {
  * Represents a sortable property of a document. 
  * 
  * @property {String} id An ID by which to identify this definition. 
- * @property {SORTING_STATE} state The current state of the sorting definition. 
- * @property {String} iconHtml An HTML literal to display as icon before the label. 
- * @property {String} localizedLabel A localized label. 
- * @property {String} localizedToolTip A localized tooltip. 
+ * @property {String | undefined} iconHtml An HTML literal to display as icon before the label. 
+ * @property {String | undefined} localizedLabel A localized label. 
+ * @property {String | undefined} localizedToolTip A localized tooltip for the icon. 
+ * @property {String | undefined} localizedToolTipSortAscending A localized tooltip for the button to sort ascending. 
+ * @property {String | undefined} localizedToolTipSortDescending A localized tooltip for the button to sort descending. 
  */
 export class SortingDefinition {
   /**
    * @param {Object} args 
    * @param {String} args.id An ID by which to identify this definition. 
-   * @param {SORTING_STATE | undefined} args.state The current state of the sorting definition. 
-   * * default `SORTING_STATE.INDETERMINATE`
    * @param {String | undefined} args.iconHtml An HTML literal to display as icon before the label. 
-   * * default `""`
    * @param {String | undefined} args.localizedLabel A localized label. 
-   * * default `""`
-   * @param {String | undefined} args.localizedToolTip A localized tooltip. 
-   * * default `""`
+   * @param {String | undefined} args.localizedToolTip A localized tooltip for the icon. 
+   * @param {String | undefined} args.localizedToolTipSortAscending A localized tooltip for the button to sort ascending. 
+   * @param {String | undefined} args.localizedToolTipSortDescending A localized tooltip for the button to sort descending. 
    */
   constructor(args = {}) {
     validateOrThrow(args, ["id"]);
 
     this.id = args.id;
-    this.state = args.state ?? SORTING_STATE.INDETERMINATE;
-    this.iconHtml = args.iconHtml ?? "";
-    this.localizedLabel = args.localizedLabel ?? "";
-    this.localizedToolTip = args.localizedToolTip ?? "";
+    this.iconHtml = args.iconHtml;
+    this.localizedLabel = args.localizedLabel;
+    this.localizedToolTip = args.localizedToolTip;
+    this.localizedToolTipSortAscending = args.localizedToolTipSortAscending;
+    this.localizedToolTipSortDescending = args.localizedToolTipSortDescending;
   }
-
-  /**
-   * Returns current state represented by a symbol as an HTML literal. 
-   * 
-   * @returns {String}
-   */
-  getSymbol() {
-    if (this.state === SORTING_STATE.ASCENDING) {
-      return '<i class="fas fa-caret-up"></i>';
-    } else if (this.state === SORTING_STATE.DESCENDING) {
-      return '<i class="fas fa-caret-down"></i>';
-    } else {
-      return "-";
-    }
-  }
-}
-
-/**
- * Represents the state a sorting definition can be in. 
- * 
- * @property {Number} INDETERMINATE The property has not yet been used in sorting. 
- * @property {Number} ASCENDING The property is used to sort in an ascending fashion. 
- * @property {Number} DESCENDING The property is used to sort in a descending fashion. 
- * 
- * @constant
- */
-export const SORTING_STATE = {
-  INDETERMINATE: 0,
-  ASCENDING: 1,
-  DESCENDING: 2,
 }
