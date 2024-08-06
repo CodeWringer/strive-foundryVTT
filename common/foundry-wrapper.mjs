0f634472b46bb745e4e0379fbc831d954cfe9a54
Nicolas 'Krubi' H. <nicharegoggl@gmail.com>
2024-08-06 11:43:43 +0200

    Issue/498 feature: Added foundry version notes to wrapper functions

--- common/foundry-wrapper.mjs
+++ common/foundry-wrapper.mjs
@@ -2,17 +2,17 @@ import { isDefined } from "../business/util/validation-utility.mjs";
 
 export default class FoundryWrapper {
   mergeObject(defaultOptions, overrides) {
-    if (isDefined(foundry) && isDefined(foundry.utils) && isDefined(foundry.utils.mergeObject)) {
+    if (isDefined(foundry) && isDefined(foundry.utils) && isDefined(foundry.utils.mergeObject)) { // Foundry 12
       return foundry.utils.mergeObject(defaultOptions, overrides);
-    } else {
+    } else { // Foundry 11
       return mergeObject(defaultOptions, overrides);
     }
   }
   
   getDice(faces, number) {
-    if (isDefined(foundry) && isDefined(foundry.dice) && isDefined(foundry.dice.terms) && isDefined(foundry.dice.terms.Die)) {
+    if (isDefined(foundry) && isDefined(foundry.dice) && isDefined(foundry.dice.terms) && isDefined(foundry.dice.terms.Die)) { // Foundry 12
       return new foundry.dice.terms.Die({ faces: faces, number: number })
-    } else {
+    } else { // Foundry 11
       return new Die({ faces: faces, number: number })
     }
   }
