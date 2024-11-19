c30241dceb6dede7a3fd7b0ee0ce683f568a5e42
Nicolas 'Krubi' H. <nicharegoggl@gmail.com>
2024-11-19 16:04:30 +0100

    Issue/526: feature: The combat tracker now supports adding the same combatant multiple times

--- presentation/combat/game-system-combat.mjs
+++ presentation/combat/game-system-combat.mjs
@@ -1,4 +1,7 @@
+import DocumentFetcher from "../../business/document/document-fetcher/document-fetcher.mjs";
+import { GENERAL_DOCUMENT_TYPES } from "../../business/document/general-document-types.mjs";
 import GameSystemWorldSettings from "../../business/setting/game-system-world-settings.mjs";
+import { ValidationUtil } from "../../business/util/validation-utility.mjs";
 
 /**
  * This class extends FoundryVTT's `Combat` document type. 
@@ -23,5 +26,40 @@ export class GameSystemCombat extends Combat {
         currentActor.actionPoints = newApCount;
       }
     }
+  }
+
+  /**
+   * Invoked when a combatant is added. 
+   * 
+   * @returns {Array<Document>}
+   * @override
+   * @see https://foundryvtt.com/api/classes/client.Combat.html#createEmbeddedDocuments 
+   */
+  async createEmbeddedDocuments(embeddedName, data, operation) {
+    const newData = data.concat([]);
+
+    for await (const entry of data) {
+      const document = await new DocumentFetcher().find({
+        id: entry.actorId,
+        documentType: GENERAL_DOCUMENT_TYPES.ACTOR,
+        includeLocked: false,
+        searchEmbedded: false,
+      });
+      const transientDocument = document.getTransientObject();
+      if (ValidationUtil.isDefined(transientDocument.initiative)
+        && transientDocument.initiative.perTurn > 1) {
+        // Insert additional combatants for this actor. 
+        for (let i = 1; i < transientDocument.initiative.perTurn; i++) {
+          newData.push({
+            actorId: entry.actorId,
+            hidden: entry.hidden,
+            sceneId: entry.sceneId,
+            tokenId: entry.tokenId,
+          });
+        }
+      }
+    }
+
+    return await super.createEmbeddedDocuments(embeddedName, newData, operation);
   }
 }
