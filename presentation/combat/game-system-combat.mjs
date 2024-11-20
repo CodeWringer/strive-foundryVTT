519cba16b3d37e2fc1beb489138a334f3093510d
Nicolas 'Krubi' H. <nicharegoggl@gmail.com>
2024-11-20 13:12:56 +0100

    Issue/543: feature: Initiative now based on a D20 roll and consecutive initiative of same token now reduced

--- presentation/combat/game-system-combat.mjs
+++ presentation/combat/game-system-combat.mjs
@@ -1,6 +1,7 @@
 import DocumentFetcher from "../../business/document/document-fetcher/document-fetcher.mjs";
 import { GENERAL_DOCUMENT_TYPES } from "../../business/document/general-document-types.mjs";
 import GameSystemWorldSettings from "../../business/setting/game-system-world-settings.mjs";
+import { ArrayUtil } from "../../business/util/array-utility.mjs";
 import { ValidationUtil } from "../../business/util/validation-utility.mjs";
 
 /**
@@ -63,5 +64,80 @@ export default class GameSystemCombat extends Combat {
     }
 
     return await super.createEmbeddedDocuments(embeddedName, newData, operation);
+  }
+
+  /**
+   * @param {Array<String>} ids Combatant IDs. 
+   * @param {Object} options 
+   * 
+   * @returns {Combat}
+   */
+  async rollInitiative(ids, options) {
+    const tokensToCombatantsMap = new Map();
+    for (const id of ids) {
+      const combatant = this.combatants.get(id);
+      const combatantIds = tokensToCombatantsMap.get(combatant.tokenId) ?? [];
+      combatantIds.push(id);
+      tokensToCombatantsMap.set(combatant.tokenId, combatantIds);
+    }
+
+    const divisorToIdsMap = new Map();
+    for (const tokenId of tokensToCombatantsMap.keys()) {
+      // Find all combatants of the token. 
+      const combatantsOfToken = this._getCombatantsOfToken(tokenId);
+      // Count the number of them that already have an initiative. 
+      let initiativeCount = 1;
+      for (const combatantOfToken of combatantsOfToken) {
+        // Avoid counting initiative of combatants that are about to be re-rolled. 
+        if (ArrayUtil.arrayContains(ids, combatantOfToken.id)) {
+          continue;
+        }
+        if (ValidationUtil.isDefined(combatantOfToken.initiative)) {
+          initiativeCount++;
+        }
+      }
+
+      const combatantIds = tokensToCombatantsMap.get(tokenId);
+      for (let i = 0; i < combatantIds.length; i++) {
+        const combatantId = combatantIds[i];
+        const divisor = initiativeCount + i;
+        const combatantIdsForDivisor = divisorToIdsMap.get(divisor) ?? [];
+        combatantIdsForDivisor.push(combatantId);
+        divisorToIdsMap.set(divisor, combatantIdsForDivisor);
+      }
+    }
+
+    let updatedCombat;
+    const initiativeFormula = CONFIG.Combat.initiative.formula;
+    for (const divisor of divisorToIdsMap.keys()) {
+      const options = {
+        formula: `(${initiativeFormula}) / ${divisor}`,
+        updateTurn: false,
+        messageOptions: {},
+      };
+      const idsToRoll = divisorToIdsMap.get(divisor);
+      updatedCombat = await super.rollInitiative(idsToRoll, options);
+    }
+
+    return updatedCombat;
+  }
+
+  /**
+   * Returns all combatants that belong to the given token. 
+   * 
+   * @param {String} tokenId ID of a token. 
+   * 
+   * @returns {Array<Combatant>}
+   * 
+   * @private
+   */
+  _getCombatantsOfToken(tokenId) {
+    const combatants = [];
+    for (const combatant of this.combatants) {
+      if (combatant.tokenId === tokenId) {
+        combatants.push(combatant);
+      }
+    }
+    return combatants;
   }
 }
