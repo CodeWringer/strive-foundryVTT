22d16309a8bb2c6521dc2e948bb3ce9ef28752a4
Nicolas 'Krubi' H. <nicharegoggl@gmail.com>
2025-09-03 00:30:09 +0200

    Version increase 1.12.0 -> 1.13.0

--- business/migration/migrators.mjs
+++ business/migration/migrators.mjs
@@ -37,6 +37,7 @@ import Migrator_1_8_0__1_9_0 from './migrators/migrator_1-8-0_1-9-0.mjs';
 import Migrator_1_9_0__1_10_0 from './migrators/migrator_1-9-0_1-10-0.mjs';
 import Migrator_1_10_0__1_11_0 from './migrators/migrator_1-10-0_1-11-0.mjs';
 import Migrator_1_11_0__1_12_0 from './migrators/migrator_1-11-0_1-12-0.mjs';
+import Migrator_1_12_0__1_13_0 from './migrators/migrator_1-12-0_1-13-0.mjs';
 
 /**
  * Defines the list of system migrators. 
@@ -84,4 +85,5 @@ export const MIGRATORS = [
   new Migrator_1_9_0__1_10_0(),
   new Migrator_1_10_0__1_11_0(),
   new Migrator_1_11_0__1_12_0(),
+  new Migrator_1_12_0__1_13_0(),
 ];
