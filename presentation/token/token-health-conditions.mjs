8713f305f688aac62368012d06d338e3ccddb681
Nicolas 'Krubi' H. <nicharegoggl@gmail.com>
2025-09-17 14:14:09 +0200

    Issue/679: fix: Token Health Conditions now always rendered above tokens

--- presentation/token/token-health-conditions.mjs
+++ presentation/token/token-health-conditions.mjs
@@ -62,7 +62,7 @@ export default class TokenHealthConditions extends TokenExtender {
   /** @override */
   async hoverOff(token) {
     if (token.actor.type === ACTOR_TYPES.PLAIN) return;
-    this._removeHoverFrom(token);
+    this._removeHover();
   }
 
   /** @override */
@@ -203,14 +203,12 @@ export default class TokenHealthConditions extends TokenExtender {
   /**
    * Removes the hover elements. 
    * 
-   * @param {Token} token 
-   * 
    * @private
    */
-  async _removeHoverFrom(token) {
-    if (ValidationUtil.isDefined(token.healthConditionHoverContainer)) {
-      token.removeChild(token.healthConditionHoverContainer);
-      token.healthConditionHoverContainer = undefined;
+  async _removeHover() {
+    if (ValidationUtil.isDefined(game.canvas.tokens.striveHoverLayer)) {
+      game.canvas.tokens.striveHoverLayer.removeChildren();
+      game.canvas.tokens.striveHoverLayer.healthConditionHoverContainer = undefined;
     }
   }
 
@@ -222,14 +220,21 @@ export default class TokenHealthConditions extends TokenExtender {
    * @private
    */
   async _addHoverTo(token) {
-    if (ValidationUtil.isDefined(token.healthConditionHoverContainer)) return;
+    // Ensure our own layer exists.
+    if (!ValidationUtil.isDefined(game.canvas.tokens.striveHoverLayer)) {
+      game.canvas.tokens.striveHoverLayer = new PIXI.Container();
+      game.canvas.tokens.addChild(game.canvas.tokens.striveHoverLayer);
+    }
+    const striveHoverLayer = game.canvas.tokens.striveHoverLayer;
+
+    if (ValidationUtil.isDefined(striveHoverLayer.healthConditionHoverContainer)) return;
     
     const healthConditions = this._getTokenHealthConditions(token);
 
     // Root container
-    token.healthConditionHoverContainer = new PIXI.Container();
-    token.addChild(token.healthConditionHoverContainer);
-
+    striveHoverLayer.healthConditionHoverContainer = new PIXI.Container();
+    striveHoverLayer.addChild(striveHoverLayer.healthConditionHoverContainer);
+    
     // This ensures the hover content to always be scaled relative to the view's 
     // current zoom level. Zooming in increases _viewPosition.scale, 
     // while zooming out reduces it. It is a float, which means when it's 1.0, 
@@ -238,7 +243,7 @@ export default class TokenHealthConditions extends TokenExtender {
 
     const style = token._getTextStyle();
     const textScale = scale; // Magic constant seems a good default text scale. 
-    let x = token.w;
+    let x = 0;
     let y = 0;
     let entriesInCurrentColumn = 0;
     let maxWidth = 0;
@@ -246,11 +251,11 @@ export default class TokenHealthConditions extends TokenExtender {
       const container = new PIXI.Container();
       container.position.set(x, y);
       let xInContainer = 0;
-      if (!ValidationUtil.isDefined(token.healthConditionHoverContainer)) {
+      if (!ValidationUtil.isDefined(striveHoverLayer.healthConditionHoverContainer)) {
         game.strive.logger.logWarn("Undefined container reference");
         continue;
       }
-      token.healthConditionHoverContainer.addChild(container);
+      striveHoverLayer.healthConditionHoverContainer.addChild(container);
 
       if (ValidationUtil.isDefined(healthCondition.iconTextureUrl)) {
         // Icon
@@ -301,5 +306,10 @@ export default class TokenHealthConditions extends TokenExtender {
         y += container.height;
       }
     }
+
+    striveHoverLayer.healthConditionHoverContainer.position.set(
+      token.x + token.w,
+      token.y
+    );
   }
 }
