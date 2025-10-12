00a171e1339a45dff283c7d8835d4ec3fffabf54
Nicolas 'Krubi' H. <nicharegoggl@gmail.com>
2025-10-12 16:17:14 +0200

    Issue/489: Shortened asset slot UUIDs
    * Considering how few asset slots a character typically has, collisions shouldn't be an issue at any time. The shorter UUID format should thus suffice.
    * And improved the readability of the UUID code a little.

--- business/util/uuid-utility.mjs
+++ business/util/uuid-utility.mjs
@@ -23,18 +23,19 @@ export const UuidUtil = {
    */
   createUUID: function (useProperFormat = false) {
     const format = useProperFormat ? this.properFormat : this.simpleFormat;
-    let d = new Date().getTime(); // Timestamp
-    let d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return format.replace(/[xy]/g, function (c) {
+    let timestamp = new Date().getTime();
+    let elapsedMicroSeconds = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0; //Time in microseconds since page-load or 0 if unsupported
+
+    return format.replace(/[xy]/g, function (char) {
       var r = Math.random() * 16; // Random number between 0 and 16
-      if (d > 0) { // Use timestamp until depleted
-        r = (d + r) % 16 | 0;
-        d = Math.floor(d / 16);
+      if (timestamp > 0) { // Use timestamp until depleted
+        r = (timestamp + r) % 16 | 0;
+        timestamp = Math.floor(timestamp / 16);
       } else { // Use microseconds since page-load if supported
-        r = (d2 + r) % 16 | 0;
-        d2 = Math.floor(d2 / 16);
+        r = (elapsedMicroSeconds + r) % 16 | 0;
+        elapsedMicroSeconds = Math.floor(elapsedMicroSeconds / 16);
       }
-      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      return (char === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
   },
 
